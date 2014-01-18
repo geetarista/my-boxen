446fe13f0faef3def922cf385511ac15c7764e20
Robby Colvin <geetarista@gmail.com>
2014-01-17 17:11:10 -0800

    Ain't nobody got time for old rubies

--- manifests/site.pp
+++ manifests/site.pp
@@ -84,8 +84,6 @@ node default {
   nodejs::module { $node_modules: }
 
   # default ruby versions
-  include ruby::1_8_7
-  include ruby::1_9_3
   include ruby::2_0_0
   class { 'ruby::global':
     version => '2.0.0'
