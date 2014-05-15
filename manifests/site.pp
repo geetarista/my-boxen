318d2186ac1530e912fff7626c20bcbc5e49279c
Robby Colvin <geetarista@gmail.com>
2014-05-15 14:40:35 -0700

    More rubies

--- manifests/site.pp
+++ manifests/site.pp
@@ -88,6 +88,7 @@ node default {
 
   # default ruby versions
   ruby::version { '1.9.3': }
+  ruby::version { '2.0.0': }
   ruby::version { '2.1.1': }
   class { 'ruby::global':
     version => '2.1.1'
